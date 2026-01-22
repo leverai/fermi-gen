d3fd451c08d6e3462235483fd1ede39b85dd7a4f
mo <mhdmartini@github.com>
2026-01-22 12:55:16 -0500

    fixup(vg): video margin

--- apps/video-gen/tests/test_video_gen.py
+++ apps/video-gen/tests/test_video_gen.py
@@ -1,3 +1,5 @@
+"""Test video generation."""
+
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -6,8 +8,13 @@ from app.generator import VideoGenerator
 
 @dataclass
 class Fermi:
+    """mock fermi object."""
+
     uid: str = '123'
-    text: str = 'What is 2+2?'
+    text: str = (
+        'What is 2+2 because two plus two is a very important '
+        'question for you to answer?'
+    )
     number: int = 4
     unit: str = 'kilometer'
 
